242a16556995857f1c6608d5bd4f7f1ca889b4bd
Shaik Basha Vali <bashavali.shaik@outlook.com>
2023-12-14 17:41:25 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleChat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
